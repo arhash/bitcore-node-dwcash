6d483f7b8e8083ffdec5c089b7954d18e77243b9
DeepWebCash <root@deepwebcash.com>
2016-11-04 15:53:03 +0800

    replace

--- replace.sh
+++ replace.sh
@@ -1,3 +1,4 @@
+sed -i '' "s/str4d/deepwebcash/g" `grep str4d -rl --exclude-dir=.git --exclude=replace.sh .`
 sed -i '' "s/ZCash/DeepWebCash/g" `grep ZCash -rl --exclude-dir=.git --exclude=replace.sh .`
 sed -i '' "s/ZCASH/DWCASH/g" `grep ZCASH -rl --exclude-dir=.git --exclude=replace.sh .`
 sed -i '' "s/z\.cash/dw\.cash/g" `grep z\.cash -rl --exclude-dir=.git --exclude=replace.sh .`
